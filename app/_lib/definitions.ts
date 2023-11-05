4410c1995b565ce52aa6153b7f0cec73ca5af653
Alexits93 <alexitspatrik@gmail.com>
2023-11-05 22:47:28 +0100

    chore: extended definitions

--- app/_lib/definitions.ts
+++ app/_lib/definitions.ts
@@ -1,6 +1,20 @@
 export type User = {
+	id: number;
 	avatar: string;
 	first_name: string;
 	last_name: string;
 	role_id: number;
+};
+
+export type Article = {
+	id: number;
+	user_id: number;
+	title: string;
+	text: string;
+	categories: string[];
+};
+
+export type Category = {
+	id: number;
+	name: string;
 };
