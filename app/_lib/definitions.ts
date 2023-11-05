c58e08bdf06bd5e49ccb0cb33638f5cd145ac696
Alexits93 <alexitspatrik@gmail.com>
2023-11-05 22:31:46 +0100

    build: setting up the basic structure

--- app/_lib/definitions.ts
+++ app/_lib/definitions.ts
@@ -1,6 +1,6 @@
 export type User = {
-  avatar: string;
-  first_name: string;
-  last_name: string;
-  role_id: number;
+	avatar: string;
+	first_name: string;
+	last_name: string;
+	role_id: number;
 };
