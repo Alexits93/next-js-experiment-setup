3f982f23751435a1e958f676f6ee00d2aed614cf
Alexits93 <alexitspatrik@gmail.com>
2023-11-06 01:23:58 +0100

    build: adding docker files

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	assetPrefix: process.env.BASE_PATH || '',
+	basePath: process.env.BASE_PATH || '',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
